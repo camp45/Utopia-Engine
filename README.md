97d06863b08a89a78d43476a04737a1e07138ec1
camp45 <campbelb6262@gmail.com>
2017-04-01 12:40:27 -0500

    Initialized main python code file. Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Utopia-Engine
-A python rendition of the board game Utopia Engine
+A python rendition of the board game Utopia Engine from Nevermore Games
+See http://nevermoregames.com/collection/utopia-engine for game info.
---
+++ UE_v1.py
@@ -0,0 +1,10 @@
+# Utopia Engine - A python intreptation of the single player game Utopia Engine from Nevermore Games.
+# (http://nevermoregames.com/collection/utopia-engine)
+
+import random
+
+def roller():
+	# Provides randomized rolls for the game
+	
+
+
